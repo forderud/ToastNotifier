6cd247faffdff90259ce82a1dd8ee8817dbd5abf
Fredrik Orderud <forderud@gmail.com>
2022-05-23 22:05:36 +0200

    Add "Generate toast" impl.

--- ToastNotifierUWP/MainPage.xaml.cs
+++ ToastNotifierUWP/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using Windows.UI.Notifications.Management;
+using Windows.UI.Xaml.Controls;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -9,14 +11,33 @@ namespace ToastNotifierUWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private UserNotificationListener m_listener;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            // get listener for the current user
+            m_listener = UserNotificationListener.Current;
         }
 
         private void btnGenerate_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
-
+            // generate a test toast
+            // SetBackgroundActivation will prevent launching a new instance of this app when the user clicks on the toast
+            new ToastContentBuilder()
+                .AddText("SW installation")
+                .AddText("Choose installation time")
+                .AddButton(new ToastButton()
+                    .SetContent("Now")
+                    .AddArgument("action", "now")
+                    .SetBackgroundActivation())
+                .AddButton(new ToastButton()
+                    .SetContent("Later")
+                    .AddArgument("action", "later")
+                    .SetBackgroundActivation())
+                .SetBackgroundActivation()
+                .Show();
         }
     }
 }
