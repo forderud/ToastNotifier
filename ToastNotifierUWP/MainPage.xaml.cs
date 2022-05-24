47e2128efcddc031a587fdeefe5f9c5c54e29824
Fredrik Orderud <forderud@gmail.com>
2022-05-24 11:19:30 +0200

    Notification listening.

--- ToastNotifierUWP/MainPage.xaml.cs
+++ ToastNotifierUWP/MainPage.xaml.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.Toolkit.Uwp.Notifications;
+using System;
+using System.Collections.Generic;
+using Windows.Foundation.Metadata;
+using Windows.UI.Notifications;
 using Windows.UI.Notifications.Management;
 using Windows.UI.Xaml.Controls;
 
@@ -16,9 +20,98 @@ namespace ToastNotifierUWP
         public MainPage()
         {
             this.InitializeComponent();
+            InitializeListener();
+        }
+
+        private async void InitializeListener()
+        {
+            if (!ApiInformation.IsTypePresent("Windows.UI.Notifications.Management.UserNotificationListener"))
+            {
+                var dialog = new ContentDialog
+                {
+                    Title = "UWP error",
+                    Content = "UserNotificationListener not supported (too old Windows version).",
+                    CloseButtonText = "Ok"
+                };
+
+                await dialog.ShowAsync();
+                return;
+            }
 
             // get listener for the current user
             m_listener = UserNotificationListener.Current;
+
+            // request access to the user's notifications (must be called from UI thread)
+            UserNotificationListenerAccessStatus accessStatus = await m_listener.RequestAccessAsync();
+            switch (accessStatus)
+            {
+                case UserNotificationListenerAccessStatus.Allowed:
+                    break; // success
+
+                case UserNotificationListenerAccessStatus.Denied:
+                    {
+                        var dialog = new ContentDialog
+                        {
+                            Title = "Error",
+                            Content = "UserNotificationListenerAccessStatus.Denied.",
+                            CloseButtonText = "Ok"
+                        };
+
+                        await dialog.ShowAsync();
+                    }
+                    return;
+
+                case UserNotificationListenerAccessStatus.Unspecified:
+                    {
+                        var dialog = new ContentDialog
+                        {
+                            Title = "Error",
+                            Content = "UserNotificationListenerAccessStatus.Unspecified. Please retry.",
+                            CloseButtonText = "Ok"
+                        };
+
+                        await dialog.ShowAsync();
+                    }
+                    return;
+            }
+
+            try
+            {
+                m_listener.NotificationChanged += Listener_NotificationChanged; // 'Element not found' exception if not running as a UWP process
+            }
+            catch (Exception ex)
+            {
+                var dialog = new ContentDialog
+                {
+                    Title = "NotificationChanged error",
+                    Content = ex.ToString(),
+                    CloseButtonText = "Ok"
+                };
+
+                await dialog.ShowAsync();
+            }
+        }
+
+        private async void Listener_NotificationChanged(UserNotificationListener sender, UserNotificationChangedEventArgs args)
+        {
+            UserNotification notif = m_listener.GetNotification(args.UserNotificationId);
+            if (notif == null)
+                return;
+
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => {
+                title.Text = notif.AppInfo.DisplayInfo.DisplayName; // application name
+
+                // accessing notif.Notification.Visual.Bindings directly will just give the same "ToastGeneric" NotificationBinding
+                NotificationBinding toastBinding = notif.Notification.Visual.GetBinding(KnownNotificationBindings.ToastGeneric);
+                if (toastBinding == null)
+                    return;
+
+                content.Text = "";
+                IReadOnlyList<AdaptiveNotificationText> elms = toastBinding.GetTextElements();
+                foreach (var elm in elms)
+                    content.Text += elm.Text + Environment.NewLine;
+
+            });
         }
 
         private void btnGenerate_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
