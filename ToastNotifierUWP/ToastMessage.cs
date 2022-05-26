220bf62af9f833ffeb218108d571b57467833e67
Fredrik Orderud <forderud@gmail.com>
2022-05-26 12:17:23 +0200

    Add toast message counter.

--- ToastNotifierUWP/ToastMessage.cs
+++ ToastNotifierUWP/ToastMessage.cs
@@ -2,12 +2,14 @@
 
 internal class ToastMessage
 {
+    static uint s_counter = 0;
+
     public static void Generate()
     {
         // generate a test toast
         // SetBackgroundActivation will prevent launching a new instance of this app when the user clicks on the toast
         new ToastContentBuilder()
-            .AddText("SW installation")
+            .AddText($"SW installation {s_counter++}")
             .AddText("Choose installation time")
             .AddButton(new ToastButton()
                 .SetContent("Now")
